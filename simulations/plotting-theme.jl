77e77b8de1b2e74e5749788abe6acf8f09b8fea5
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-09-07 20:00:29 +0200

    Minor fixes

--- simulations/plotting-theme.jl
+++ simulations/plotting-theme.jl
@@ -5,34 +5,34 @@ colors = [:black, :red, :blue, :green, :purple]
 marker = [:rect, :dtriangle, :diamond, :xcross, :star4]
 
 paper_theme = Theme(
-    backgroundcolor = :white,
-    resolution      = (1600, 1200),
-    fontsize        = 48,
-    figure_padding  = (30, 80, 30, 30),
+    backgroundcolor  = :white,
+    resolution       = (1600, 1200),
+    fontsize         = 48,
+    figure_padding   = (30, 80, 30, 30),
 
     palette = (
-        color       = colors,
-        strokecolor = colors,
-        marker      = marker
+        color        = colors,
+        strokecolor  = colors,
+        marker       = marker
     ),
 
     Axis = (
-        xgridstyle  = :dash,
-        ygridstyle  = :dash,
-        xgridwidth = 3.0,
-        ygridwidth = 3.0
+        xgridstyle   = :dash,
+        ygridstyle   = :dash,
+        xgridwidth   = 3.0,
+        ygridwidth   = 3.0
     ),
 
     Lines = (
-        linewidth   = 2.5,
-        linestyle   = :dash
+        linewidth    = 2.5,
+        linestyle    = :dash
     ),
 
     Scatter = (
-        markersize  = 30,
-        strokewidth = 1.5,
-        color       = :transparent,
-        cycle       = Cycle([:strokecolor, :marker], covary = true)
+        markersize   = 30,
+        strokewidth  = 1.5,
+        color        = :transparent,
+        cycle        = Cycle([:strokecolor, :marker], covary = true)
     ),
 
     Legend = (
